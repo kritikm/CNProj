c89613ab8594f8b3c3679e886d7ba0b0245a61bc
KritikM <kritik.mathur@learner.manipal.edu>
2016-09-30 12:22:58 +0530

    Updated ENQ_LIB.h with some documentation and more code

--- ENQ_LIB.h
+++ ENQ_LIB.h
@@ -1,67 +1,73 @@
-#pragma once
+/*
+	Class definitions:-
+		Question
+		Game
+	Utility Functions:-
+		bool isExists(int *, int, int);
+		void getRandoms(int *, int);
+*/
+
+
 #include<string>
 #include<fstream>
 #include<list>
+#include<cstdlib>
+#include<ctime>
+#define GAME_LENGTH 10
 
 using namespace std;
 
-class Option
+
+bool isExists(int * arr, int size, int key)
 {
-public:
-	string option;
-	bool isCorrect;
+	for (int i = 0; i < size; i++)
+		if (arr[i] == key)
+			return true;
+	return false;
+}
 
-	Option(string toOption, bool toIsCorrect)
-	{
-		option = toOption;
-		isCorrect = toIsCorrect;
-	}
+void getRandoms(int * arr, int size)
+{
+	srand(time(0));
 
-	Option()
+	for (int i = 0; i < size; i++)
 	{
-		option = "";
-		isCorrect = false;
+		int gen;
+
+		do {
+			gen = rand() % 50;
+		} while (isExists(arr, i, gen));
+
+		arr[i] = gen;
 	}
-};
+}
 
 class Question
 {
-public:
-	int qID;
+	/*
+		Each question will be an object of this class. A question has a string which holds the question itself and 4 instances of Option type
+		Creates a question and saves it to a file
+		Author: Kritik
+	*/
+
+private:
 	string question;
-	Option optionA, optionB, optionC, optionD;
+	string optionA, optionB, optionC, optionD;
+	char correctAnswer;
 
-	Question() { question = ""; }
-
-	Question(string toQuestion, string toA, string toB, string toC, string toD, char isCorrect)
+public:
+	static int n_Questions;
+	
+	Question(string toQuestion = "", string toA = "", string toB = "", string toC = "", string toD = "", char isCorrect = 'a')
 	{
-		Question c;
+		
 		question = toQuestion;
-		optionA.option = toA;
-		optionB.option = toB;
-		optionC.option = toC;
-		optionD.option = toD;
-
-		switch (isCorrect)
-		{
-		case 'a':
-			optionA.isCorrect = true;
-			break;
-
-		case 'b':
-			optionB.isCorrect = true;
-			break;
-
-		case 'c':
-			optionC.isCorrect = true;
-			break;
-
-		case 'd':
-			optionD.isCorrect = true;
-
-		default:
-			break;
-		}
+		optionA = toA;
+		optionB = toB;
+		optionC = toC;
+		optionD = toD;
+		correctAnswer = isCorrect;
+		
 	}
 
 	void saveToFile(string filePath)
@@ -69,11 +75,28 @@ public:
 		fstream qFile;
 		qFile.open(filePath, ios::ate | ios::binary);
 		qFile.write((char *)this, sizeof(Question));
+		n_Questions++;
 	}
 };
 
 class Game
 {
+
+	/*
+		This sets up the game environment. Generates a sequence of random numbers and fetches the corresponding questions from a file.
+		Also handles the game scores and the UI
+		Author: Kritik
+	*/
+
 	list<Question> quiz;
+	
+	Game(string filePath)
+	{
+		int sequence[GAME_LENGTH];
+		getRandoms(sequence, GAME_LENGTH);
+
+		//get questions from 'filePath'
+	}
+
 	
 };
