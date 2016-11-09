e5491de507834bad2ea2bc820c4246dc4a6c7595
Nimish Agrawal <nimish.agrawal21@gmail.com>
2016-11-10 01:03:29 +0530

    Adding python script to reformat questions files
    Instructions:
    >> Paste the script into questions directory
    >> Run the script
    >> New files will be in a sub-directory, call 'Formmatted'

---
+++ reformat_questions.py
@@ -0,0 +1,31 @@
+import os
+import io
+
+# directory = os.getcwd()
+# print os.listdir(directory)
+
+try:
+    os.mkdir("Formatted")
+    for i in range(1, 11):
+        filename = str(i) + ".txt"
+        # print filename
+        file_old = open(filename, 'r')
+        file_new = open("Formatted/" + filename, 'w')
+        counter = 0
+        for each_line in file_old:
+            # print each_line
+            if len(each_line) == 1:
+                continue
+            print each_line + str(len(each_line))
+            if '.' in each_line:
+                each_line = each_line[3:]
+            if ':' in each_line:
+                each_line = each_line[1].lower()
+
+            file_new.write(each_line)
+        file_old.close()
+        file_new.close()
+
+except IOError as err:
+    print 'File handling error:'
+    print str(err)
